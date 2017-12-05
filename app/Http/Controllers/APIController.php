3b023d900224f79812cd8408d2a801c0fc26d6ca
ahmadbasir <akhmadbasir5@gmail.com>
2017-12-05 19:39:31 +0700

    update API

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -7,10 +7,10 @@ use Illuminate\Http\Request;
 
 class APIController extends Controller
 {
-    public function ganti_user($id, $type)
+    public function ganti_user(Request $request, $id, $type)
     {
         $user = User::find($id);
-        $user->user_type = $type;
+        $user->user_type = $request->$type;
         $user->update();
         return $user;
     }
