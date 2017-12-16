944814b4b19492603ab375b4092af2862f298942
ahmadbasir <akhmadbasir5@gmail.com>
2017-12-16 22:18:10 +0700

    update title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Management Tugas</title>
+    <title>Management Mhs</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
