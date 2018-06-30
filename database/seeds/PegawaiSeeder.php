051c79cb032808e503604c88f0dce00f09409bd6
ahmadbasir <akhmadbasir5@gmail.com>
2018-06-30 22:53:41 +0700

    update

--- database/seeds/PegawaiSeeder.php
+++ database/seeds/PegawaiSeeder.php
@@ -16,7 +16,7 @@ class PegawaiSeeder extends Seeder
       DB::table('pegawais')->insert([
         'nama'          => str_random(10),
         'no_ktp'        => str_random(12),
-        'gaji'          => rand(10)*100000,
+        'gaji'          => rand(1,10)*100000,
         'status_pegawai'=> str_random(3),
         'created_at'    => $carbon->now()->toDateString(),
         'updated_at'    => $carbon->now()->toDateString(),
